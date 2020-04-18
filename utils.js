363206aebe4a10e33a7cda0e3402938f89416801
bobthebuilder9548 <62567001+bobthebuilder9548@users.noreply.github.com>
2020-04-18 14:02:56 -0700

    date utilities

---
+++ utils.js
@@ -0,0 +1,12 @@
+export function getISOfromDatepicker(date) {
+	let iso = "T00:00:00+00:00"
+	return date + iso
+}
+
+//date is of type iso string
+export function getAge(date) {
+	let parsed = Date.parse(date)
+	let diff = Date.now() - parsed
+	let ageDate = new Date(diff)
+	return Math.abs(ageDate.getUTCFullYear() - 1970)
+}
