ddf5405b18beb2e6e73b35814ce841e42d145d9d
Harald Napp <harald.napp@googlemail.com>
2020-06-29 20:16:08 +0200

    show detail fields in victim view

--- src/components/VictimDetails.js
+++ src/components/VictimDetails.js
@@ -1,17 +1,23 @@
 import React, { useRef, useEffect } from 'react';
 import './ViewComponent.scss';
+import langs from '../data/languages.js';
 
 const VictimDetails = (props) => {
-
+	
+	//console.log(props)
+	//alert(props.data[0].languages_spoken)
+	let language = props.data && langs.filter(lang => lang.code === props.data[0].languagues_spoken);
+	//alert(language[0].code);
+	
 	return (
 		<div className='details-container'>
-			<p> Nationality: </p>
-			<p> Health Status </p>
-			<p> Health Issues </p>
-			<p> Languages Spoken</p>
-			<p> Profession </p>
-			<p> About the Victim </p>
-			<p> Additional Information </p>
+			       {/* <p> Nationality </p>*/}
+			<p> Health Status: {props.data && props.data[0].health_status} </p>
+			<p> Health Issues: {props.data && props.data[0].health_issues}  </p>
+			<p> Languages Spoken: {language && language[0] && language[0].name}	</p>			
+			<p> Profession: {props.data && props.data[0].profession}</p>
+			<p> About the Victim: {props.data && props.data[0].about_the_victim} </p>
+			<p> Additional Information: {props.data && props.data[0].additional_information} </p>
 		</div>
 	)
 }
