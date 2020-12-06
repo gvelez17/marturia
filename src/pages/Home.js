75e3453ecccf8e5aab6fbf2b520018da310e8224
Golda Velez <gvelez17@gmail.com>
2020-12-06 15:01:16 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,15 +11,20 @@ function Home() {
     <MainLayout>
       <div className="home page">
         <div className="wrapper">
-          <p>
-		  <span className="title">The Testimony Database</span><br/><br/>
-		  <u>Mission</u><br/><br/>
-		  
-		  Providing free, secure, easy to use, online robust technology to help identify missing persons and loved ones, living or deceased. No missing person should be left unaccounted for. Missing someone and not being able to see them is one of the worst feelings. <br/><br/> This website aims to connect families to loved ones who have been reported missing or deceased. Our mission is to subdue the pain of family members who have suffered the loss or disappearance of their loved ones. <br/><br/>
-		We hope that you put your sincere trust in us and will let us help you find the people you hold close to your heart. Simply fill out the information in the “Victim”, tab to search for the missing person, or report a missing person by filling out the information in the “Submit” tab. With this tool we hope that your dearest ones will be found. 
-
-         
-		  </p>
+	   <p>
+              <span className="title">The Testimony Database</span><br/>
+</p>
+   <p>
+Welcome. Every person is important here, regardless of birthplace, race, religion, gender or language.  Our mission is to raise up the stories of those who are silenced by force.
+</p><p>
+This database records testimonies about enforced disappearances and extrajudicial violence.  That is, if someone is harmed or kidnapped by force and cannot go to their government for protection - either because the government itself is involved, corrupt, or ineffective, their story belongs here.  In cases where the judiciary is or may be complicit, cases that have had "trials" may also be included.
+</p><p>
+You may enter your own story, one you know of first hand, or information from news reports.  You will be prompted for the source of your information and also will have the option to withhold specific names and contacts.  You may also work anonymously with a volunteer of <a href="http://RaisetheVoices.org">Raise the Voices</a>. Please do NOT enter any information that could endanger an individual, we cannot ensure that information entered is absolutely secure. In this case you should contact a volunteer over Signal - see <a href="https://raisethevoices.org">website</a>.
+</p>
+<p>
+We provide as much sources and credentials as possible for the histories here; it will be indicated what the sources or corroborations are.  Journalists and NGOs are welcome to contact us for reports or data exports at <a href="mailto:info@raisethevoices.org">info@raisethevoices.org</a> Volunteers are also welcome!
+          </p>
+	  
         </div>
       </div>
     </MainLayout>
